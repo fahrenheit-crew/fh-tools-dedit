9b347b74507039bc84a0e3834521eb2d46a04135
Fran Kelava <fran.kelava@outlook.com>
2025-06-25 17:22:43 +0200

    Bump System.CommandLine integration.

--- main.cs
+++ main.cs
@@ -15,45 +15,63 @@ using Fahrenheit.Core.FFX;
 
 namespace Fahrenheit.Tools.DEdit;
 
+internal enum FhDEditMode {
+    ReadCharsets = 1,
+    Compile      = 2,
+    Decompile    = 3
+}
+
 internal class Program {
     static void Main(string[] args) {
         Console.WriteLine($"{nameof(Fahrenheit)}.{nameof(DEdit)}\n");
         Console.WriteLine($"Started with args: {string.Join(' ', args)}\n");
 
-        Option<FhDEditMode> optMode     = new Option<FhDEditMode>("--mode", "Select the DEdit operating mode.");
-        Option<string>      optDefNs    = new Option<string>("--ns", "Set the namespace of the resulting C# file, if reading a charset.");
-        Option<string>      optFilePath = new Option<string>("--src", "Set the path to the source file.");
-        Option<string>      optDestPath = new Option<string>("--dest", "Set the folder where the C#/text file should be written.");
-        Option<FhLangId>    optCharSet  = new Option<FhLangId>("--cs", "Set the charset that should be used for the input file.");
+        Option<FhDEditMode> opt_mode    = new Option<FhDEditMode>("--mode") { Description = "Select the DEdit operating mode." };
+        Option<string>      opt_ns      = new Option<string>     ("--ns"  ) { Description = "Set the namespace of the resulting C# file, if reading a charset." };
+        Option<string>      opt_src     = new Option<string>     ("--src" ) { Description = "Set the path to the source file." };
+        Option<string>      opt_dest    = new Option<string>     ("--dest") { Description = "Set the folder where the C#/text file should be written." };
+        Option<FhLangId>    opt_charset = new Option<FhLangId>   ("--cs"  ) { Description = "Set the charset that should be used for the input file." };
 
-        optMode.IsRequired     = true;
-        optFilePath.IsRequired = true;
-        optDestPath.IsRequired = true;
+        opt_mode.Required = true;
+        opt_src .Required = true;
+        opt_dest.Required = true;
 
-        RootCommand rootCmd = new RootCommand("Perform various operations on FFX dialogue files and character sets.") {
-            optMode,
-            optDefNs,
-            optFilePath,
-            optDestPath,
-            optCharSet
+        RootCommand root_cmd = new RootCommand("Perform various operations on FFX dialogue files and character sets.") {
+            opt_mode,
+            opt_ns,
+            opt_src,
+            opt_dest,
+            opt_charset
         };
 
-        rootCmd.SetHandler(DEditMain, new DEditArgsBinder(
-            optMode,
-            optDefNs,
-            optFilePath,
-            optDestPath,
-            optCharSet));
+        ParseResult argparse_result = root_cmd.Parse(args);
 
-        rootCmd.Invoke(args);
+        FhDEditMode mode              = argparse_result.GetValue(opt_mode);
+        string      src_path          = argparse_result.GetValue(opt_src)  ?? "";
+        string      dest_path         = argparse_result.GetValue(opt_dest) ?? "";
+        string      default_namespace = argparse_result.GetValue(opt_ns)   ?? "";
+        FhLangId    lang_id           = argparse_result.GetValue(opt_charset);
+
+        Stopwatch perfSwatch = Stopwatch.StartNew();
+
+        if (!File.Exists(src_path))
+            throw new Exception("E_INVALID_PATH");
+
+        switch (mode) {
+            case FhDEditMode.ReadCharsets: DEditReadCharset(src_path, dest_path, default_namespace); break;
+            case FhDEditMode.Decompile: DEditDecompile(lang_id, src_path, dest_path); break;
+        }
+
+        perfSwatch.Stop();
+        Console.WriteLine($"PERF: Operation complete in {perfSwatch.ElapsedMilliseconds} ms");
         return;
     }
 
-    static void DEditReadCharset() {
-        string sfn = Path.GetFileName(DEditConfig.SrcPath);
-        string dfn = Path.Join(DEditConfig.DestPath, $"{sfn}-{Guid.NewGuid()}.g.cs");
-        string ns  = DEditConfig.CharsetReader?.DefaultNamespace ?? throw new Exception("FH_E_MISSING_NAMESPACE: Specify --ns at the command line.");
-        string cs  = FhCharsetGenerator.EmitCharset(DEditConfig.SrcPath, ns);
+    static void DEditReadCharset(string src_path, string dest_path, string? default_namespace = default) {
+        string sfn = Path.GetFileName(src_path);
+        string dfn = Path.Join(dest_path, $"{sfn}-{Guid.NewGuid()}.g.cs");
+        string ns  = default_namespace ?? throw new Exception("FH_E_MISSING_NAMESPACE: Specify --ns at the command line.");
+        string cs  = FhCharsetGenerator.EmitCharset(src_path, ns);
 
         using (FileStream fs = File.Open(dfn, FileMode.CreateNew)) {
             using (StreamWriter sw = new StreamWriter(fs)) {
@@ -65,19 +83,17 @@ internal class Program {
         Console.WriteLine($"Charset {sfn}: Output is at {dfn}.");
     }
 
-    static void DEditDecompile() {
-        FhLangId cs = DEditConfig.Decompile!.CharSet;
-
-        string sfn       = Path.GetFileName(DEditConfig.SrcPath);
+    static void DEditDecompile(FhLangId lang_id, string src_path, string dest_path) {
+        string sfn       = Path.GetFileName(src_path);
         bool   isMDict   = sfn == "macrodic.dcp";
         string dfnSuffix = isMDict ? "FFX_MACRODICT" : "FFX_DIALOGUE";
-        string dfn       = Path.Join(DEditConfig.DestPath, $"{sfn}-{Guid.NewGuid()}.{dfnSuffix}");
+        string dfn       = Path.Join(dest_path, $"{sfn}-{Guid.NewGuid()}.{dfnSuffix}");
 
-        ReadOnlySpan<byte> dialogue = File.ReadAllBytes(DEditConfig.SrcPath);
+        ReadOnlySpan<byte> dialogue = File.ReadAllBytes(src_path);
 
         string diastr = isMDict
-            ? DEditDecompileMacroDict(dialogue, cs)
-            : DEditDecompileDialogue(dialogue, cs);
+            ? DEditDecompileMacroDict(dialogue, lang_id)
+            : DEditDecompileDialogue(dialogue, lang_id);
 
         using (FileStream fs = File.Open(dfn, FileMode.CreateNew)) {
             using (StreamWriter sw = new StreamWriter(fs)) {
@@ -124,23 +140,5 @@ internal class Program {
         }
 
         return sb.ToString();
-    }
-
-    static void DEditMain(FhDEditArgs config) {
-        DEditConfig.CLIRead(config);
-        Stopwatch perfSwatch = Stopwatch.StartNew();
-
-        if (!File.Exists(DEditConfig.SrcPath))
-            throw new Exception("E_INVALID_PATH");
-
-        switch (DEditConfig.Mode) {
-            case FhDEditMode.ReadCharsets:
-                DEditReadCharset(); break;
-            case FhDEditMode.Decompile:
-                DEditDecompile(); break;
-        }
-
-        perfSwatch.Stop();
-        Console.WriteLine($"PERF: Operation complete in {perfSwatch.ElapsedMilliseconds} ms");
     }
 }
